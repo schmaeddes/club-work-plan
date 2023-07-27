83a887aad3e740be282cf21901df5e0d145f5b5f
Matthias Kaiser <maettes@MacBook-Air-von-Jessica.local>
2023-07-27 13:58:01 +0200

    added first admin table

--- club-work-plan/EventsTable.php
+++ club-work-plan/EventsTable.php
@@ -1,48 +1,66 @@
-<?php
+<?php 
+
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
 
 class EventsTable extends WP_List_Table {
 
-/**
- * Constructor, we override the parent to pass our own arguments
- * We usually focus on three parameters: singular and plural labels, as well as whether the class supports AJAX.
- */
- function __construct() {
-    parent::__construct( array(
-   'singular'=> 'wp_cwp_event', //Singular label
-   'plural' => 'wp_cwp_events', //plural label, also this well be one of the table css class
-   'ajax'   => false //We won't support Ajax for this table
-   ) );
- }
+    private $table_data;
 
+    // Here we will add our code
+    function prepare_items() {
+        $this->table_data = $this->get_table_data();
 
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = array();
+        $primary  = 'event_name';
+        $this->_column_headers = array($columns, $hidden, $sortable, $primary);
+        
+        $this->items = $this->table_data;
+    }
 
- /**
- * Add extra markup in the toolbars before or after the list
- * @param string $which, helps you decide if you add the markup after (bottom) or before (top) the list
- */
-function extra_tablenav( $which ) {
-   if ( $which == "top" ){
-      //The code that goes before the table is here
-      echo"Hello, I'm before the table";
-   }
-   if ( $which == "bottom" ){
-      //The code that goes after the table is there
-      echo"Hi, I'm after the table";
-   }
-}}
+    // Get table data
+    private function get_table_data() {
+        global $wpdb;
 
-/**
- * Define the columns that are going to be used in the table
- * @return array $columns, the array of columns to use with the table
- */
-function get_columns() {
-    return $columns= array(
-       'id'=>__('ID'),
-       'event_name'=>__('Name'),
-       'description'=>__('Description'),
-       'dat_of_event'=>__('Date of event'),
-       'date_of_creation'=>__('Date of creation')
-    );
- }
+        $table = $wpdb->prefix . 'cwp_events';
+
+        return $wpdb->get_results(
+            "SELECT * from {$table}",
+            ARRAY_A
+        );
+    }
+
+    function column_default($item, $column_name) {
+          switch ($column_name) {
+                case 'id':
+                case 'event_name':
+                case 'description':
+                case 'date_of_event':
+                default:
+                    return $item[$column_name];
+          }
+    }
+
+    function column_cb($item) {
+        return sprintf(
+                '<input type="checkbox" name="element[]" value="%s" />',
+                $item['id']
+        );
+    }
+
+    // Define table columns
+    function get_columns() {
+        $columns = array(
+            'cb'            => '<input type="checkbox" />',
+            'event_name'    => __('Event Name', 'supporthost-cookie-consent'),
+            'description'   => __('Description', 'supporthost-cookie-consent'),
+            'date_of_event' => __('Date of event', 'supporthost-cookie-consent')
+        );
+        return $columns;
+    }
+}
 
 ?>
